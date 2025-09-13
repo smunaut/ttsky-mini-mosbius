2f30c95fb3ef77437496d091b54b11a58ddcee83
Sylvain Munaut <tnt@246tNt.com>
2025-09-13 16:35:43 +0200

    tcl: Include internals of the standard cells when running LVS
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- tcl/lvs.tcl
+++ tcl/lvs.tcl
@@ -1,5 +1,6 @@
 set layout [readnet spice $project.lvs.spice]
 set schem  [readnet verilog /dev/null]
+readnet spice $::env(PDK_ROOT)/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice $schem
 readnet spice   ../xschem/simulation/mosbius.spice $schem
 readnet verilog ../src/ctrl_top.synth.v            $schem
 readnet verilog ../src/project.v                   $schem
